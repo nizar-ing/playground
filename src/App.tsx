b5771cdb0d3d1c8bd0d755a6e91e0ea4467ee9e8
nizarIng <nizar.ilahi@gmail.com>
2024-01-18 18:27:40 +0100

    Exercise: Using the same semantic, create a custom usePosts hook using react-query.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css';
 import TodoList from "./react-query/TodoList";
+import PostList from "./react-query/PostList";
 
 function App() {
-  return <TodoList />;
+  return <PostList />;
 }
 
 export default App;
