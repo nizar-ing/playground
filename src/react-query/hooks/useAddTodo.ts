020eb4e7485840260539f56f240a1810fb0042f9
nizarIng <nizar.ilahi@gmail.com>
2024-01-19 20:41:33 +0100

    Creating a reusable HTTP service.

--- src/react-query/hooks/useAddTodo.ts
+++ src/react-query/hooks/useAddTodo.ts
@@ -1,18 +1,16 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
-import {Todo} from "./useTodos";
 import {CACHE_KEY_TODOS} from "../constants";
-import APIClient from "../services/APIClient";
+import todoService, {Todo} from "../services/todoService";
 
 interface AddTodoContext {
     previousTodos: Todo[];
 }
 
-const apiClient = new APIClient<Todo>('/todos');
 const useAddTodo = (onAdd: () => void) => {
     const queryClient = useQueryClient();
 
     return useMutation<Todo, Error, Todo, AddTodoContext>({
-        mutationFn: apiClient.post,
+        mutationFn: todoService.post,
         onMutate: (newTodo: Todo) => {
             const previousTodos = queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
             queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos = []) => [newTodo, ...todos]);
--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,18 +1,11 @@
 import {useQuery} from "@tanstack/react-query";
 import {CACHE_KEY_TODOS} from "../constants";
-import APIClient from "../services/APIClient";
+import todoService, {Todo} from "../services/todoService";
 
-export interface Todo {
-    id: number;
-    title: string;
-    userId: number;
-    completed: boolean;
-}
-const apiClient = new APIClient<Todo>('/todos');
 const useTodos = () => {
     return useQuery<Todo[], Error>({
         queryKey: CACHE_KEY_TODOS,
-        queryFn: apiClient.getAll,
+        queryFn: todoService.getAll,
         staleTime: 10 * 1000 // setting up the stale time per query
     });
 };
